235a06294abeaa6f635bccb174370e2fa2e72301
smsutherland <sagan@farthinghalearms.com>
2024-02-28 13:07:03 -0500

    create random sentence and measure wpm

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,29 @@ use crossterm::{
     style::Print,
     terminal::{disable_raw_mode, enable_raw_mode},
 };
-use std::io::Write;
+use std::{io::Write, time::Instant};
 use strcursor::StrCursor;
+
+mod language {
+    pub const ENGLISH_1K: &str = include_str!("../data/english_1k.list");
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut term = Term::stdout();
     enable_raw_mode()?;
 
-    let s = "Word1 Word2";
-    write!(term, "{}", style(s).color256(59))?;
+    let s = create_sentence();
+    write!(term, "{}", style(&s).color256(59))?;
     execute!(term, MoveToColumn(0))?;
     let mut letters = String::with_capacity(s.len());
-    let mut chars = StrCursor::new_at_start(s);
+    let mut chars = StrCursor::new_at_start(&s);
+    let mut start_time = None;
     'main: while letters.len() != s.len() {
         loop {
             let c2 = term.read_key()?;
+            if start_time.is_none() {
+                start_time = Some(Instant::now());
+            }
             match c2 {
                 console::Key::Shift => {}
                 console::Key::Backspace => {
@@ -46,7 +54,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     }
+    let end_time = Instant::now();
+    let start_time = start_time.unwrap();
+    let duration = end_time - start_time;
+    let wpm = (letters.len() as f64 / 5.0) / (duration.as_secs_f64() / 60.0);
     disable_raw_mode()?;
     println!();
+    println!("WPM: {wpm:.2}");
     Ok(())
+}
+
+fn create_sentence() -> String {
+    let words = language::ENGLISH_1K.split('\n').collect::<Vec<_>>();
+    let count = words.len();
+    let mut sentence = String::new();
+    for i in 0..10 {
+        if i != 0 {
+            sentence.push(' ');
+        }
+        let word = words[fastrand::usize(..count)];
+        sentence.push_str(word);
+    }
+    sentence
 }
